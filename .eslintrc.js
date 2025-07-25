718e0dcef61e4265611f83ef04602b6438f0702f
carlospugae <carlospugae@gmail.com>
2025-07-24 23:43:38 -0300

    chore(eslint): integrate @tanstack/eslint-plugin-query

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
+import pluginQuery from '@tanstack/eslint-plugin-query';
+
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: {
+    '@tanstack/query': pluginQuery,
+  },
+  rules: {
+    '@tanstack/query/exhaustive-deps': 'error',
+  },
   overrides: [
     {
       files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
