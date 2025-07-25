96722e085cf3519e5fde229aa76f53ca4b88c1dc
carlospugae <carlospugae@gmail.com>
2025-07-24 22:42:51 -0300

    fix(*): import coinmarketcap api key directly in the fetchCryptos function

--- src/hooks/use-fetch-cryptos.ts
+++ src/hooks/use-fetch-cryptos.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { CryptoToken } from '@/types/types';
+import { CMC_API_KEY } from '@env';
 
 export const CMC_DOMAIN = 'https://pro-api.coinmarketcap.com';
 export const CMC_ENDPOINT = '/v1/cryptocurrency/listings/latest';
@@ -9,7 +10,6 @@ export const COINMARKETCAP_URL = `${CMC_DOMAIN}${CMC_ENDPOINT}${CMC_QUERY_PARAMS
 
 export const fetchCryptos = async (): Promise<CryptoToken[]> => {
   // Import CMC_API_KEY here so jest mock is used in tests
-  const { CMC_API_KEY } = require('@env');
   const response = await fetch(COINMARKETCAP_URL, {
     headers: {
       'X-CMC_PRO_API_KEY': CMC_API_KEY,
